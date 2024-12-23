eb68133cf49190dc14d04f6f2a308f9ea0afa657
Sathishperugu1999 <sathishperugu1999@gmail.com>
2024-12-24 00:41:29 +0530

    latest changes

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sathishdcr/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -it -p 8000:8000 sathishdcr/simple-python-flask-app:latest
